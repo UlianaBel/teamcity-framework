fb55f77e616d18a26fc5110e3103a8778d4c145b
UlianBel <ulianbel@gmail.com>
2024-10-08 21:55:23 +0400

    Add config, spec, base tests

---
+++ src/main/java/com/example/teamc/api/enums/Endpoint.java
@@ -0,0 +1,16 @@
+package com.example.teamc.api.enums;
+
+import com.example.teamc.api.models.BaseModel;
+import com.example.teamc.api.models.BuildType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Endpoint {
+    BUILD_TYPES("/app/rest/buildTypes", BuildType.class);
+
+    private final String url;
+    private final Class<? extends BaseModel> modelClass;
+
+}
